0aef3802fd589ca869d6e8e7805a29b53f9e3e0e
Sanjayk46 <138973406+Sanjayk46@users.noreply.github.com>
2024-12-25 12:32:23 +0530

    Update ProjectBox.jsx

--- src/components/ProjectBox.jsx
+++ src/components/ProjectBox.jsx
@@ -7,15 +7,15 @@ const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
     EcommerceDesc : "This e-commerce website provides users with a seamless online shopping experience for a wide range of electronics, specifically laptops and mobiles. The platform features an intuitive interface that allows users to browse various models, view detailed product descriptions, and compare items easily. Customers can make secure purchases through an integrated payment gateway (Razorpay), ensuring a smooth transaction process.",
     EcommerceGithub : "https://github.com/Sanjayk46/Ecom-frontend",
-    EcommerceWebsite : "http://ecom-mern-app.s3-website.eu-north-1.amazonaws.com/",
+    EcommerceWebsite : "https://skecom.netlify.app/",
 
     FoodAppDesc : "This food delivery app provides users with a seamless online food ordering experience. Featuring an intuitive interface, the app allows users to browse through a variety of restaurants, explore detailed menus, customize their orders, and place them with ease. Secure payments are handled through an integrated payment gateway (Razorpay), ensuring a smooth and reliable transaction process from start to finish.",
     FoodAppGithub : "https://github.com/Sanjayk46/food-app-frontend",
-    FoodAppWebsite : "http://food-mern-app.s3-website.eu-north-1.amazonaws.com/",
+    FoodAppWebsite : "https://skrestaurant-food.netlify.app/",
 
     ChatAppDesc:"This chat application offers real-time communication powered by WebSocket, allowing users to connect effortlessly, send instant messages, and share media seamlessly. With a user-friendly interface and real-time updates, users can communicate effectively and enjoy a dynamic chatting experience.",
     ChatAppGithub:"https://github.com/Sanjayk46/chat-app-frontend",
-    ChatAppWebsite:"http://chat-mern-app.s3-website.eu-north-1.amazonaws.com/",
+    ChatAppWebsite:"https://mernchatapps.netlify.app/",
     
     TaskManagementDesc:"This task management app allows users to seamlessly manage their tasks by providing features for updating and editing existing tasks. Users can easily create new tasks.The intuitive interface ensures a smooth user experience, making it easy to keep track of progress and prioritize workloads.",
     TaskManagementGithub:"https://github.com/Sanjayk46/Task-app-frontend",
